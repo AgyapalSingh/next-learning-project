bd2432bd0e6103a5143f7f74f8eff1084fe3d24b
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-05-21 11:21:29 +0530

    Different fonts Testing

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,22 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bowlby_One_SC, Proza_Libre, Rammetto_One } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const bowlbyOneSC = Bowlby_One_SC({
+  weight: ['400'],
+  variable: "--font-bowlby-one-sc",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+const rammettoOne = Rammetto_One({
+  weight: ['400'],
+  variable: "--font-rammetto-one",
+  subsets: ["latin"],
+});
+
+const prozaLibre = Proza_Libre({
+  weight: ['400', '500', '600', '700', '800'],
+  variable: "--font-proza-libre",
   subsets: ["latin"],
 });
 
@@ -19,7 +28,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={` ${bowlbyOneSC.variable} ${prozaLibre.variable} ${rammettoOne.variable}`}>
         {children}
       </body>
     </html>
