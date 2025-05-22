8fb22a9c3adf37a1e04a594847f62ff9f63c1990
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-05-22 17:35:18 +0530

    Project comment out for testing

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -16,100 +16,100 @@ const projects = [
 ];
 
 export default function AnimatedSections() {
-  useEffect(() => {
-    const sections = document.querySelectorAll("section");
-    const images = document.querySelectorAll(".bg");
-    const headings = gsap.utils.toArray(".animated-section-heading");
-    const outerWrappers = gsap.utils.toArray(".outer");
-    const innerWrappers = gsap.utils.toArray(".inner");
-    const scrollHint = document.querySelector(".scroll-hint");
+  // useEffect(() => {
+  //   const sections = document.querySelectorAll("section");
+  //   const images = document.querySelectorAll(".bg");
+  //   const headings = gsap.utils.toArray(".animated-section-heading");
+  //   const outerWrappers = gsap.utils.toArray(".outer");
+  //   const innerWrappers = gsap.utils.toArray(".inner");
+  //   const scrollHint = document.querySelector(".scroll-hint");
 
-    const splitHeadings = headings.map((heading) => {
-      return new SplitType(heading, {
-        types: "words, chars",
-        tagName: "span",
-      });
-    });
+  //   const splitHeadings = headings.map((heading) => {
+  //     return new SplitType(heading, {
+  //       types: "words, chars",
+  //       tagName: "span",
+  //     });
+  //   });
 
-    let currentIndex = -1;
-    let animating;
-    const wrap = gsap.utils.wrap(0, sections.length);
+  //   let currentIndex = -1;
+  //   let animating;
+  //   const wrap = gsap.utils.wrap(0, sections.length);
 
-    gsap.set(outerWrappers, { yPercent: 100 });
-    gsap.set(innerWrappers, { yPercent: -100 });
+  //   gsap.set(outerWrappers, { yPercent: 100 });
+  //   gsap.set(innerWrappers, { yPercent: -100 });
 
-    function gotoSection(index, direction) {
-      index = wrap(index);
-      animating = true;
-      const fromTop = direction === -1;
-      const dFactor = fromTop ? -1 : 1;
+  //   function gotoSection(index, direction) {
+  //     index = wrap(index);
+  //     animating = true;
+  //     const fromTop = direction === -1;
+  //     const dFactor = fromTop ? -1 : 1;
 
-      const tl = gsap.timeline({
-        defaults: { duration: 1.25, ease: "power1.inOut" },
-        onComplete: () => (animating = false),
-      });
+  //     const tl = gsap.timeline({
+  //       defaults: { duration: 1.25, ease: "power1.inOut" },
+  //       onComplete: () => (animating = false),
+  //     });
 
-      if (currentIndex >= 0) {
-        gsap.set(sections[currentIndex], { zIndex: 0 });
-        tl.to(images[currentIndex], { yPercent: -15 * dFactor }).set(
-          sections[currentIndex],
-          { autoAlpha: 0 }
-        );
-      }
+  //     if (currentIndex >= 0) {
+  //       gsap.set(sections[currentIndex], { zIndex: 0 });
+  //       tl.to(images[currentIndex], { yPercent: -15 * dFactor }).set(
+  //         sections[currentIndex],
+  //         { autoAlpha: 0 }
+  //       );
+  //     }
 
-      gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
-      tl.fromTo(
-        [outerWrappers[index], innerWrappers[index]],
-        { yPercent: (i) => (i ? -100 * dFactor : 100 * dFactor) },
-        { yPercent: 0 },
-        0
-      )
-        .fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0)
-        .fromTo(
-          splitHeadings[index]?.chars || [],
-          { autoAlpha: 0, yPercent: 150 * dFactor },
-          {
-            autoAlpha: 1,
-            yPercent: 0,
-            duration: 1,
-            ease: "power2",
-            stagger: { each: 0.02, from: "random" },
-          },
-          0.2
-        );
+  //     gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
+  //     tl.fromTo(
+  //       [outerWrappers[index], innerWrappers[index]],
+  //       { yPercent: (i) => (i ? -100 * dFactor : 100 * dFactor) },
+  //       { yPercent: 0 },
+  //       0
+  //     )
+  //       .fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0)
+  //       .fromTo(
+  //         splitHeadings[index]?.chars || [],
+  //         { autoAlpha: 0, yPercent: 150 * dFactor },
+  //         {
+  //           autoAlpha: 1,
+  //           yPercent: 0,
+  //           duration: 1,
+  //           ease: "power2",
+  //           stagger: { each: 0.02, from: "random" },
+  //         },
+  //         0.2
+  //       );
 
-      if (scrollHint) {
-        if (index === 0) {
-          scrollHint.classList.add("visible");
-        } else {
-          scrollHint.classList.remove("visible");
-        }
-      }
+  //     if (scrollHint) {
+  //       if (index === 0) {
+  //         scrollHint.classList.add("visible");
+  //       } else {
+  //         scrollHint.classList.remove("visible");
+  //       }
+  //     }
 
-      currentIndex = index;
-    }
+  //     currentIndex = index;
+  //   }
 
-    requestAnimationFrame(() => {
-      Observer.create({
-        type: "wheel,touch,pointer",
-        wheelSpeed: -1,
-        onDown: () =>
-          !animating && currentIndex > 0 && gotoSection(currentIndex - 1, -1),
-        onUp: () =>
-          !animating &&
-          currentIndex < sections.length - 1 &&
-          gotoSection(currentIndex + 1, 1),
-        tolerance: 10,
-        preventDefault: true,
-      });
+  //   requestAnimationFrame(() => {
+  //     Observer.create({
+  //       type: "wheel,touch,pointer",
+  //       wheelSpeed: -1,
+  //       onDown: () =>
+  //         !animating && currentIndex > 0 && gotoSection(currentIndex - 1, -1),
+  //       onUp: () =>
+  //         !animating &&
+  //         currentIndex < sections.length - 1 &&
+  //         gotoSection(currentIndex + 1, 1),
+  //       tolerance: 10,
+  //       preventDefault: true,
+  //     });
 
-      gotoSection(0, 1);
-    });
-  }, []);
+  //     gotoSection(0, 1);
+  //   });
+  // }, []);
 
   return (
     <>
-      {projects.map((project, i) => (
+      {/* {projects.map((project, i) => (
         <section
           key={project.id}
           className={`animated-section ${
@@ -137,7 +137,7 @@ export default function AnimatedSections() {
             </div>
           )}
         </section>
-      ))}
+      ))} */}
     </>
   );
 }
