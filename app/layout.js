4950aa3ed88cbb22b20152ba51af693402ef2571
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-05-22 17:12:44 +0530

    Smoothe Scrolling added using lenis

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,22 @@
 import { Bowlby_One_SC, Proza_Libre, Rammetto_One } from "next/font/google";
 import "./globals.css";
 import Header from "./Header/Header";
+import SmoothScrolling from "./Components/SmoothScrolling";
 
 const bowlbyOneSC = Bowlby_One_SC({
-  weight: ['400'],
+  weight: ["400"],
   variable: "--font-bowlby-one-sc",
   subsets: ["latin"],
 });
 
-
 const rammettoOne = Rammetto_One({
-  weight: ['400'],
+  weight: ["400"],
   variable: "--font-rammetto-one",
   subsets: ["latin"],
 });
 
 const prozaLibre = Proza_Libre({
-  weight: ['400', '500', '600', '700', '800'],
+  weight: ["400", "500", "600", "700", "800"],
   variable: "--font-proza-libre",
   subsets: ["latin"],
 });
@@ -29,9 +29,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={` ${bowlbyOneSC.variable} ${prozaLibre.variable} ${rammettoOne.variable}`}>
-        <Header/>
-        {children}
+      <body
+        className={` ${bowlbyOneSC.variable} ${prozaLibre.variable} ${rammettoOne.variable}`}
+      >
+        <Header />
+        <SmoothScrolling> {children}</SmoothScrolling>
       </body>
     </html>
   );
